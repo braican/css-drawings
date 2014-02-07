df19f73d5933126826eb88b27eada03a22007817
Nick Braica <nbraica@tankdesign.com>
2014-02-07 09:53:26 -0500

    removing Atari and Nyan cat, 'cause I didn't do them

--- Nyan/nyan.js
+++
@@ -1,7 +0,0 @@
-//Meh, not listening to the grief you might give me over not css. 
-
-function songOfTheNyanPeople () {
-    var audio = document.getElementsByTagName("audio")[0];
-    audio.play();
-    audio.loop = true;
-}
